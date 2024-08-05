ef6416c6a993fbd1bb6a39d19146adfa5754f0ec
Elias Holzer <elias@vvvv.org>
2024-08-05 18:08:42 +0200

    Removes animation from auto hide window

--- WinFormsUI/Docking/DockPanel.AutoHideWindow.cs
+++ WinFormsUI/Docking/DockPanel.AutoHideWindow.cs
@@ -2,6 +2,7 @@ using System;
 using System.Windows.Forms;
 using System.Drawing;
 using System.ComponentModel;
+using System.Threading;
 
 namespace WeifenLuo.WinFormsUI.Docking
 {
@@ -38,15 +39,12 @@ namespace WeifenLuo.WinFormsUI.Docking
             private const int ANIMATE_TIME = 100;    // in mini-seconds
             #endregion
 
-            private Timer m_timerMouseTrack;
+
             protected SplitterBase m_splitter { get; private set; }
 
             public AutoHideWindowControl(DockPanel dockPanel)
             {
                 m_dockPanel = dockPanel;
-
-                m_timerMouseTrack = new Timer();
-                m_timerMouseTrack.Tick += new EventHandler(TimerMouseTrack_Tick);
 
                 Visible = false;
                 m_splitter = DockPanel.Theme.Extender.WindowSplitterControlFactory.CreateSplitterControl(this);
@@ -57,7 +55,6 @@ namespace WeifenLuo.WinFormsUI.Docking
             {
                 if (disposing)
                 {
-                    m_timerMouseTrack.Dispose();
                 }
                 base.Dispose(disposing);
             }
@@ -155,13 +152,6 @@ namespace WeifenLuo.WinFormsUI.Docking
                 get { return ActiveContent == null ? DockState.Unknown : ActiveContent.DockHandler.DockState; }
             }
 
-            private bool m_flagAnimate = true;
-            private bool FlagAnimate
-            {
-                get { return m_flagAnimate; }
-                set { m_flagAnimate = value; }
-            }
-
             private bool m_flagDragging = false;
             internal bool FlagDragging
             {
@@ -178,154 +168,21 @@ namespace WeifenLuo.WinFormsUI.Docking
 
             private void AnimateWindow(bool show)
             {
-                if (!FlagAnimate && Visible != show)
+                if (Visible != show)
                 {
                     Visible = show;
                     return;
                 }
-
-                Parent.SuspendLayout();
-
-                Rectangle rectSource = GetRectangle(!show);
-                Rectangle rectTarget = GetRectangle(show);
-                int dxLoc, dyLoc;
-                int dWidth, dHeight;
-                dxLoc = dyLoc = dWidth = dHeight = 0;
-                if (DockState == DockState.DockTopAutoHide)
-                    dHeight = show ? 1 : -1;
-                else if (DockState == DockState.DockLeftAutoHide)
-                    dWidth = show ? 1 : -1;
-                else if (DockState == DockState.DockRightAutoHide)
-                {
-                    dxLoc = show ? -1 : 1;
-                    dWidth = show ? 1 : -1;
-                }
-                else if (DockState == DockState.DockBottomAutoHide)
-                {
-                    dyLoc = (show ? -1 : 1);
-                    dHeight = (show ? 1 : -1);
-                }
-
-                if (show)
-                {
-                    Bounds = DockPanel.GetAutoHideWindowBounds(new Rectangle(-rectTarget.Width, -rectTarget.Height, rectTarget.Width, rectTarget.Height));
-                    if (Visible == false)
-                        Visible = true;
-                    PerformLayout();
-                }
-
-                SuspendLayout();
-
-                LayoutAnimateWindow(rectSource);
-                if (Visible == false)
-                    Visible = true;
-
-                int speedFactor = 1;
-                int totalPixels = (rectSource.Width != rectTarget.Width) ?
-                    Math.Abs(rectSource.Width - rectTarget.Width) :
-                    Math.Abs(rectSource.Height - rectTarget.Height);
-                int remainPixels = totalPixels;
-                DateTime startingTime = DateTime.Now;
-                while (rectSource != rectTarget)
-                {
-                    DateTime startPerMove = DateTime.Now;
-
-                    rectSource.X += dxLoc * speedFactor;
-                    rectSource.Y += dyLoc * speedFactor;
-                    rectSource.Width += dWidth * speedFactor;
-                    rectSource.Height += dHeight * speedFactor;
-                    if (Math.Sign(rectTarget.X - rectSource.X) != Math.Sign(dxLoc))
-                        rectSource.X = rectTarget.X;
-                    if (Math.Sign(rectTarget.Y - rectSource.Y) != Math.Sign(dyLoc))
-                        rectSource.Y = rectTarget.Y;
-                    if (Math.Sign(rectTarget.Width - rectSource.Width) != Math.Sign(dWidth))
-                        rectSource.Width = rectTarget.Width;
-                    if (Math.Sign(rectTarget.Height - rectSource.Height) != Math.Sign(dHeight))
-                        rectSource.Height = rectTarget.Height;
-
-                    LayoutAnimateWindow(rectSource);
-                    if (Parent != null)
-                        Parent.Update();
-
-                    remainPixels -= speedFactor;
-
-                    while (true)
-                    {
-                        TimeSpan time = new TimeSpan(0, 0, 0, 0, ANIMATE_TIME);
-                        TimeSpan elapsedPerMove = DateTime.Now - startPerMove;
-                        TimeSpan elapsedTime = DateTime.Now - startingTime;
-                        if (((int)((time - elapsedTime).TotalMilliseconds)) <= 0)
-                        {
-                            speedFactor = remainPixels;
-                            break;
-                        }
-                        else
-                            speedFactor = remainPixels * (int)elapsedPerMove.TotalMilliseconds / (int)((time - elapsedTime).TotalMilliseconds);
-                        if (speedFactor >= 1)
-                            break;
-                    }
-                }
-                ResumeLayout();
-                Parent.ResumeLayout();
-            }
-
-            private void LayoutAnimateWindow(Rectangle rect)
-            {
-                Bounds = DockPanel.GetAutoHideWindowBounds(rect);
-
-                Rectangle rectClient = ClientRectangle;
-
-                if (DockState == DockState.DockLeftAutoHide)
-                    ActivePane.Location = new Point(rectClient.Right - 2 - DockPanel.Theme.Measures.AutoHideSplitterSize - ActivePane.Width, ActivePane.Location.Y);
-                else if (DockState == DockState.DockTopAutoHide)
-                    ActivePane.Location = new Point(ActivePane.Location.X, rectClient.Bottom - 2 - DockPanel.Theme.Measures.AutoHideSplitterSize - ActivePane.Height);
-            }
-
-            private Rectangle GetRectangle(bool show)
-            {
-                if (DockState == DockState.Unknown)
-                    return Rectangle.Empty;
-
-                Rectangle rect = DockPanel.AutoHideWindowRectangle;
-
-                if (show)
-                    return rect;
-
-                if (DockState == DockState.DockLeftAutoHide)
-                    rect.Width = 0;
-                else if (DockState == DockState.DockRightAutoHide)
-                {
-                    rect.X += rect.Width;
-                    rect.Width = 0;
-                }
-                else if (DockState == DockState.DockTopAutoHide)
-                    rect.Height = 0;
-                else
-                {
-                    rect.Y += rect.Height;
-                    rect.Height = 0;
-                }
-
-                return rect;
             }
 
             private void SetTimerMouseTrack()
             {
                 if (ActivePane == null || ActivePane.IsActivated || FlagDragging)
                 {
-                    m_timerMouseTrack.Enabled = false;
                     return;
                 }
 
-                // start the timer
-                int hovertime = SystemInformation.MouseHoverTime ;
-
-                // assign a default value 400 in case of setting Timer.Interval invalid value exception
-                if (hovertime <= 0)
-                    hovertime = 400;
-
-                m_timerMouseTrack.Interval = 2 * (int)hovertime;
-                m_timerMouseTrack.Enabled = true;
+                SynchronizationContext.Current.Post(_ => TimerMouseTrack_Tick(), null);
             }
 
             protected virtual Rectangle DisplayingRectangle
@@ -361,9 +218,7 @@ namespace WeifenLuo.WinFormsUI.Docking
 
                 if (!DockHelper.IsDockStateAutoHide(ActiveContent.DockHandler.DockState))
                 {
-                    FlagAnimate = false;
                     ActiveContent = null;
-                    FlagAnimate = true;
                 }
             }
 
@@ -372,14 +227,13 @@ namespace WeifenLuo.WinFormsUI.Docking
                 SetTimerMouseTrack();
             }
 
-            private void TimerMouseTrack_Tick(object sender, EventArgs e)
+            private void TimerMouseTrack_Tick()
             {
                 if (IsDisposed)
                     return;
 
                 if (ActivePane == null || ActivePane.IsActivated)
                 {
-                    m_timerMouseTrack.Enabled = false;
                     return;
                 }
 
@@ -392,7 +246,6 @@ namespace WeifenLuo.WinFormsUI.Docking
                 if (!ClientRectangle.Contains(ptMouseInAutoHideWindow) && !rectTabStrip.Contains(ptMouseInDockPanel))
                 {
                     ActiveContent = null;
-                    m_timerMouseTrack.Enabled = false;
                 }
             }
 
